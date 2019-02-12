096834131c4da1fbee68ed3b0c6558b62ada8db8
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2019-02-12 12:55:34 +0800

    Updated Exercises File

---
+++ wk-02/exercises/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Week 02 Exercises</title>
+</head>
+<body>
+<h1>Exercises to Practice JS</h1>
+<p>
+    Find these at <a href="http://<?=$_SERVER['SERVER_NAME'];?>/AJG/Learning/wk-02/exercises.php">
+        <?= "http://".$_SERVER["SERVER_NAME"];?>/AJG/Learning/wk-02/exercises.php
+    </a>
+</p>
+<p>
+    Find the demo files at <a href="https://github.com/">
+        https://www.github.com/
+    </a>
+</p>
+<h3>Exercise 01</h3>
+<ul>
+    <li>HTML Filename: wk02-ex01.html</li>
+    <li>JS Filename: n/a</li>
+</ul>
+<p>Create a new HTMl file, as given above.</p>
+<p>Add a JavaScript script section and then write the JS to do the following:</p>
+<ol>
+    <li>define a variable called fahrenheit, with a default value of -40.0</li>
+    <li>define a variable called celcius which calculates the value of (fahrenheit minus 32) times 5 divide 9</li>
+    <li>outputs the results to the console in a form:<br/><pre>Fahrenheit: -40.0 to Celcius: -40.0</pre></li>
+</ol>
+
+<h3>Exercise 02</h3>
+<ul>
+    <li>HTML Filename: wk02-ex02.html</li>
+    <li>JS Filename: n/a</li>
+</ul>
+<p>Create a new HTMl file, as given above.</p>
+<p>Add a JavaScript script section and then write the JS to do the following:</p>
+<ol>
+    <li>prints ‘Hello World’ to the console</li>
+</ol>
+
+<h3>Exercise 03</h3>
+<ul>
+    <li>HTML Filename: wk02-ex03.html</li>
+    <li>JS Filename: n/a</li>
+</ul>
+<p>Create a new HTMl file, as given above.</p>
+<p>Add a JavaScript script section and then write the JS to do the following:</p>
+<ol>
+    <li>defines a number called maxNum with a value 10</li>
+    <li>uses a loop that adds all the numbers up</li>
+    <li>prints the sum of the numbers</li>
+    </li>
+</ol>
+
+
+<h3>Exercise 04</h3>
+<ul>
+    <li>HTML Filename: wk02-ex04.html</li>
+    <li>JS Filename: n/a</li>
+</ul>
+<p>Create a new HTMl file, as given above.</p>
+<p>Write a program that prints a multiplication table for numbers up to 12.</p>
+<p>Add a JavaScript script section and then write the JS to do the following:</p>
+<ol>
+    <li>defines a variable multiplier and sets the value 12</li>
+    <li>create a loop that counts from 1 to 12</li>
+    <li>... calculates count multiplied by multiplier</li>
+    <li>... displays <code>1  *  12 = 12</code>, <code>2  *  12 = 24</code>, etc</li>
+    <li>end of loop</li>
+</ol>
+
+</body>
+</html>
