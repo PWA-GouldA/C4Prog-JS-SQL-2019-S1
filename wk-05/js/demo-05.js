5d4e346254e2b15383cfef03dc2ce535e196ffc5
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2019-03-05 10:57:20 +0800

    Week 05 - Demo 05 base code

---
+++ wk-05/js/demo-05.js
@@ -0,0 +1,13 @@
+/**
+ * Javascript Week 05 - Demo 05
+ *
+ * Filename: js/demo-05.js
+ * Author:   Adrian Gould
+ * Date:     2019-02-26
+ *
+ */
+
+let doc = document;
+let givenName = doc.getElementById('givenName');
+
+
