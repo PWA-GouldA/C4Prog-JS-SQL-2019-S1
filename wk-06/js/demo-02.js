9468cffa436e2b6a4add37ec77903d241c63cc03
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2019-03-12 09:57:54 +0800

    Week 06 - Demo 02 Code

--- wk-06/js/demo-02.js
+++ wk-06/js/demo-02.js
@@ -1,9 +1,28 @@
 /**
- * Javascript Week 06 - Demo 00
+ * Javascript Week 06 - Demo 02
  *
- * Filename: js/demo-00.js
+ * Filename: js/demo-02.js
  * Author:   Adrian Gould
  * Date:     2019-03-12
  *
  */
 
+// Grab the Godfather... (DOM)
+let doc=document;
+let formDemo = doc.getElementById('formDemo');
+
+// listen for the SAVE button
+formDemo.addEventListener('submit', function(event)
+{
+    // get the first name field
+    let firstName = doc.getElementById('firstName')
+    // get the second name field
+    let lastName = doc.getElementById('lastName')
+    // get the first name results area
+    // get the last name results area
+
+
+    // put first name into the first name results area
+    // put surname into the surname results area
+
+}); // end event listener
