d3e7ec44c6a6682761b6762004b90668e4ecbbbc
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2019-03-19 10:14:03 +0800

    Week 07 / Demo 02 JS, HTML and PHP

---
+++ wk-07/js/demo-02.js
@@ -0,0 +1,34 @@
+/**
+ * Javascript Week 07 - Demo 02
+ *
+ * Filename: js/demo-02.js
+ * Author:   Adrian Gould
+ * Date:     2019-03-19
+ *
+ */
+
+let doc = document;
+let theForm = doc.getElementById('demoForm');
+
+theForm.addEventListener('submit', function (event) {
+    let givenName = doc.getElementById('givenName').value;
+    let givenNameError = doc.getElementById('givenNameError');
+
+    givenNameError.innerHTML = '';
+
+    if (isEmpty(givenName)) {
+        givenNameError.innerHTML = '<p class="text-danger">Please enter a given name.</p>';
+        event.preventDefault();
+    }
+
+    // all ok, so submit will happen
+});
+
+function isEmpty(data) {
+    // if (data>''){
+    //     return false
+    // }
+    // return true;
+    return data > '' ? false : true;
+}
+
